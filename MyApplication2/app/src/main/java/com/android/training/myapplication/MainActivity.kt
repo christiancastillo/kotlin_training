3406938796f01b17fd09cbfd3be12f8b8ff68b06
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-12-28 00:38:34 -0600

    -

--- MyApplication2/app/src/main/java/com/android/training/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/android/training/myapplication/MainActivity.kt
@@ -9,25 +9,33 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var userInput: EditText? = null
-    private var button: Button? = null
-    private var textView: TextView? = null
+    //private var userInput: EditText? = null
+    //private var button: Button? = null
+    //private var textView: TextView? = null
     private var numTimesClicked: Int = 0
     private var texto: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_button_click)
-        userInput = findViewById<EditText>(R.id.editTextPersonName)
-        button = findViewById<Button>(R.id.buttonCounter)
-        textView = findViewById<TextView>(R.id.textViewCounter)
+
+
+        val userInput: EditText = findViewById<EditText>(R.id.editTextPersonName)
+        val button: Button = findViewById<Button>(R.id.buttonCounter)
+        val textView: TextView = findViewById<TextView>(R.id.textViewCounter)
+        texto = userInput?.getText().toString()
+        //textView?.setText("")
+        textView.text = "" // es lo mismo que textView?.setText("")
+        userInput.setText("")
 
         //texto = userInput?.getText().toString()
-        textView?.setMovementMethod(ScrollingMovementMethod())
-        button?.setOnClickListener(object : View.OnClickListener{
+        textView.setMovementMethod(ScrollingMovementMethod())
+        //textView?.movementMethod = ScrollingMovementMethod()
+        button.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
-                numTimesClicked += 1
-                textView?.append("\n the button got tapped $numTimesClicked times. \t Information: ${userInput?.getText().toString()}.") //agrega una nueva linea
+                textView.append(userInput.text) //agrega una nueva linea
+                textView.append("\n")
+                userInput.setText("")
             }
 
         })
