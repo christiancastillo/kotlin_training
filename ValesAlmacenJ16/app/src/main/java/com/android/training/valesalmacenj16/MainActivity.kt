5e63012ba88f5d4d30916c317dce2501fc4cb251
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-01-02 23:14:48 -0600

    -

--- ValesAlmacenJ16/app/src/main/java/com/android/training/valesalmacenj16/MainActivity.kt
+++ ValesAlmacenJ16/app/src/main/java/com/android/training/valesalmacenj16/MainActivity.kt
@@ -4,17 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var buttonSearch: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) //1
-        setContentView(R.layout.activity_main) //2
-        buttonSearch = findViewById(R.id.buttonSearch) //3: Definicion de variables
-        buttonSearch.setOnClickListener {
-            val searchMedicineActivity = Intent(this, SearchMedicineList::class.java) //3: Definicion de variables e intents
-            startActivity(searchMedicineActivity)
+        try{
+            super.onCreate(savedInstanceState) //1
+            setContentView(R.layout.activity_main) //2
+            buttonSearch = findViewById(R.id.buttonSearch) //3: Definicion de variables
+            buttonSearch.setOnClickListener {
+                val searchMedicineActivity = Intent(this, SearchMedicineActivity::class.java) //3: Definicion de variables e intents
+                startActivity(searchMedicineActivity)
+            }
+        } catch(e: Exception){
+            Toast.makeText(this,"Error: "+e.message,Toast.LENGTH_LONG).show()
         }
     }
 }
