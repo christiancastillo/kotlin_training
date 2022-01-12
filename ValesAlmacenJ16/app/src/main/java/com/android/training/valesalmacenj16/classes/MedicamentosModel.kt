4d2c836e839cd3173630a6b5591890b00c9b2aea
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-01-11 21:15:13 -0600

    .

--- ValesAlmacenJ16/app/src/main/java/com/android/training/valesalmacenj16/classes/MedicamentosModel.kt
+++ ValesAlmacenJ16/app/src/main/java/com/android/training/valesalmacenj16/classes/MedicamentosModel.kt
@@ -7,3 +7,11 @@ data class MedicamentosModel (
     val presentacion: String,
     val tipo: String
         )
+
+/*
+* Ligas de interes:
+
+https://www.baeldung.com/kotlin/json-convert-data-class
+https://www.tutorialspoint.com/how-to-read-files-from-assets-on-android-using-kotlin
+https://stackoverflow.com/questions/45176941/how-to-parse-local-json-file-in-assets
+https://stackoverflow.com/questions/23073627/json-array-to-auto-complete-text-view-in-android*/
