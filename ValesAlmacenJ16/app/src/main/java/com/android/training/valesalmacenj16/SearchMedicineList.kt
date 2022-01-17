a2e143e3876db8239db2588124abda6606a7ebc5
Christian Yamil Castillo <cyccov@gmail.com>
2022-01-17 09:36:46 -0600

    -

--- ValesAlmacenJ16/app/src/main/java/com/android/training/valesalmacenj16/SearchMedicineList.kt
+++ ValesAlmacenJ16/app/src/main/java/com/android/training/valesalmacenj16/SearchMedicineList.kt
@@ -3,6 +3,7 @@ package com.android.training.valesalmacenj16
 import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.KeyListener
 import android.util.Log
 import android.widget.ArrayAdapter
 import com.android.training.valesalmacenj16.classes.MedicamentosModel
@@ -20,46 +21,48 @@ class SearchMedicineList : AppCompatActivity() {
         try{
             super.onCreate(savedInstanceState)
             binding = ActivitySearchMedicineListBinding.inflate(layoutInflater)
-            //setContentView(R.layout.activity_search_medicine_list)
             setContentView(binding.getRoot())
             var utilsJSON = JsonUtils()
             val jsonFileString = utilsJSON.getJsonDataFromAssets(getApplicationContext(),"medicamentos.json")
             val gson = Gson()
             val listMedicamentosType = object : TypeToken<List<MedicamentosModel>>(){}.getType()
             val medicamentos: List<MedicamentosModel> = gson.fromJson(jsonFileString,listMedicamentosType)
-            Log.i(TAG,"******************************DATOS DEL JSON******************************")
-            Log.i(TAG, medicamentos[0].presentacion)
-            Log.i(TAG, medicamentos[0].clave)
-            Log.i(TAG, medicamentos[0].descr)
-
             var arrayDescripcion : List<String> = listOf()
-            var arrayPresentacion : List<String> = listOf()
+            var arrayPresentacion: List<String> = listOf()
             var arrayClave : List<String> = listOf()
             var i : Int = 0
+            var descString : String = ""
+            var descPresentacion: String = ""
+            var descClave : String = ""
+
             while(i < medicamentos.size){
-                Log.i(TAG,"medicamentos.size: ${medicamentos.size}")
-                Log.i(TAG, "valor de i: ${i}")
                 arrayDescripcion = listOf(medicamentos[i].descr)
-                arrayPresentacion = listOf(medicamentos[i].presentacion)
+                arrayPresentacion  = listOf(medicamentos[i].presentacion)
                 arrayClave = listOf(medicamentos[i].clave)
 
-//                if (arrayDescripcion[i] == binding.aCTVDescripcion.getText().toString()){
-//                    binding.textViewClaveJSON.setText(arrayClave[i])
-//                    binding.textViewPresentacionJSON.setText(arrayPresentacion[i])
-//                    break
-//                }
+                Log.i(TAG,"Info: ${binding.aCTVDescripcion.getText().toString()}")
+                if (arrayDescripcion[i].contains(binding.aCTVDescripcion.getText().toString())){
+                    descClave = arrayClave[i]
+                    descPresentacion = arrayPresentacion[i]
+                    break
+                }
                 Log.i(TAG,"******************************************************")
                 Log.i(TAG,"valor de i: ${i}, descripcion: ${medicamentos[i].descr}, presentacion: ${medicamentos[i].presentacion}, clave: ${medicamentos[i].clave}")
                 Log.i(TAG,"******************************************************")
                 i++
             }
-
-            //TODO: Implementar método para convertir de un JSON a un objeto de la clase y también alimentar un arreglo
-
             adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayDescripcion)
-            Log.i(TAG,arrayDescripcion[0])
             binding.aCTVDescripcion.setThreshold(1) //empieza a trabajar desde el primer caracter
             binding.aCTVDescripcion.setAdapter(adapter)
+            binding.aCTVDescripcion.setText(binding.aCTVDescripcion.getAdapter().getItem(0).toString());
+            binding.aCTVDescripcion.setOnItemClickListener { parent, view, position, id ->
+                //TODO: Falta implementar código para listener cuando se da click en item.
+            }
+
+            //binding.textViewPresentacionJSON.setText(descPresentacion)
+            //binding.textViewClaveJSON.setText(descClave)
+
+            //TODO: Implementar método para convertir de un JSON a un objeto de la clase y también alimentar un arreglo
         } catch(e: Exception){
             Log.e("ONCREATE MEDTHOD:","********************************************")
             e.printStackTrace()
